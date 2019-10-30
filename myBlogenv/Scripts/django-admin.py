6e07051e4cf90aa7fee5aae56a288ab5b5069ae4
joshuacode1 <zuma4good@gmail.com>
2019-10-30 12:39:04 +0100

    git commit

---
+++ myBlogenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\semicolon\python\django\djangoprojects\myblog\myblogenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
